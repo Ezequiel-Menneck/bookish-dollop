c6a1d52da33956946a20bb92eaa870757309f56d
menneck <zeeeee.peeeeetryyy@gmail.com>
2022-07-14 18:00:31 -0300

    feat: improvement for name of the archives

--- src/routes/calendar.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { handleGetBook } from '../services/calendar/calendar'
-
-const router = express.Router()
-
-router.get('/calendar', handleGetBook)
-
-export default router
--- src/routes/user.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { handleGetBook } from '../services/user/user'
-
-const router = express.Router()
-
-router.get('/user', handleGetBook)
-
-export default router
