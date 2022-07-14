8ffc68393bacb6484b018f7ac6c753ad7f0fba12
menneck <zeeeee.peeeeetryyy@gmail.com>
2022-07-14 18:00:51 -0300

    feat: improvement on imports and route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import express, { Request, Response } from 'express'
-import user from './routes/user'
-import dates from './routes/calendar'
+import express from 'express'
+import user from './routes/UserRoute'
+import calendar from './routes/CalendarRoute'
 
 const app = express()
 
 app.use(express.json())
-app.use('/api', user)
-app.use('/api', dates)
+app.use('/api', user, calendar)
 
 app.listen(3003, () => {
   console.log("Application listening at http://localhost:3003")
