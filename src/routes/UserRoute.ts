0cdec9d1aa369f347562e060218e7cbbc3bb51d7
menneck <zeeeee.peeeeetryyy@gmail.com>
2022-07-18 16:01:59 -0300

    feat: change the import for controller

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,15 +1,17 @@
-import express from "express";
+import express from 'express';
 import {
   getUserById,
   getAllUsers,
   addUser,
   deleteUser,
-} from "./../services/UserService";
+  updateUser,
+} from './../controller/UserService';
 const router = express.Router();
 
-router.get("/users", getAllUsers);
-router.get("/users/:id", getUserById);
-router.post("/users/add", addUser);
-router.delete("/users/:id", deleteUser);
+router.get('/users', getAllUsers);
+router.get('/users/:id', getUserById);
+router.post('/users/add', addUser);
+router.delete('/users/:id', deleteUser);
+router.put('/users/:id', updateUser);
 
 export default router;
