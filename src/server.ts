8e2862c64a3e337ae78c96cd208360476bff4fa9
menneck <zeeeee.peeeeetryyy@gmail.com>
2022-08-01 22:24:40 -0300

    feat: add cors for application

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
-import express from 'express'
-import UserRoute from './routes/UserRoute'
-import CalendarRoute from './routes/CalendarRoute'
+import express from 'express';
+import UserRoute from './routes/UserRoute';
+import CalendarRoute from './routes/CalendarRoute';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use('/api', UserRoute, CalendarRoute)
+const allowedOrigins = ['http://localhost:3003'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
+app.use(express.json());
+app.use('/api', UserRoute, CalendarRoute);
 
 app.listen(3003, () => {
-  console.log("Application listening at http://localhost:3003")
-})
+  console.log('Application listening at http://localhost:3003');
+});
