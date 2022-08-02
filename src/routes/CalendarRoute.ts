a35bdfc96b10ca4134a2f05a7f9fd9fef9066812
menneck <zeeeee.peeeeetryyy@gmail.com>
2022-08-01 22:25:21 -0300

    feat: add new routes rotes for crud operations

--- src/routes/CalendarRoute.ts
+++ src/routes/CalendarRoute.ts
@@ -1,8 +1,16 @@
 import express from 'express';
-import { addSchedule } from '../controller/CalendarController';
+import {
+  addSchedule,
+  deleteSchedule,
+  getAllSchedules,
+  updateSchedule,
+} from '../controller/CalendarController';
 
 const router = express.Router();
 
 router.post('/calendar', addSchedule);
+router.get('/calendar', getAllSchedules);
+router.put('/calendar/:id', updateSchedule);
+router.delete('/calendar/:id', deleteSchedule);
 
 export default router;
