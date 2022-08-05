f8d54b09488ae1a8d229da7d6729a53f1dc3c01b
menneck <zeeeee.peeeeetryyy@gmail.com>
2022-08-04 22:31:51 -0300

    feat: use joi to validate the schema

--- src/controller/CalendarController.ts
+++ src/controller/CalendarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { prismaClient } from '../database/prismaCliente';
+import { calendarValidator } from '../validators/CallendarValidator';
 
 type CreateSchedule = {
   client: string;
@@ -16,22 +17,37 @@ type ServiceTypes = {
 export const addSchedule = async (req: Request, res: Response) => {
   const { client, gender, date, time }: CreateSchedule = req.body;
   const { service_name, price }: ServiceTypes = req.body.service.create;
-  const schedule = await prismaClient.schedules.create({
-    data: {
-      client,
-      gender,
-      date,
-      time,
-      service: {
-        create: {
-          service_name,
-          price,
+
+  const { error } = calendarValidator.validate(req.body);
+  const valid = error == null;
+
+  if (valid) {
+    console.log('Valido');
+    const schedule = await prismaClient.schedules.create({
+      data: {
+        client,
+        gender,
+        date,
+        time,
+        service: {
+          create: {
+            service_name,
+            price,
+          },
         },
       },
-    },
-  });
+      include: { service: true },
+    });
 
-  return res.status(201).json(schedule);
+    return res.status(201).json(schedule);
+  } else {
+    console.log('Invalido');
+    const { details } = error;
+    //@ts-ignore
+    const message = details.map(i => i.message).join(',');
+
+    return res.status(422).json({ error: message });
+  }
 };
 
 export const getAllSchedules = async (req: Request, res: Response) => {
@@ -47,23 +63,35 @@ export const updateSchedule = async (req: Request, res: Response) => {
   const { client, gender, date, time }: CreateSchedule = req.body;
   const { service_name, price }: ServiceTypes = req.body.service.create;
 
-  const newSchedule = await prismaClient.schedules.update({
-    where: { id },
-    data: {
-      client,
-      gender,
-      date,
-      time,
-      service: {
-        create: {
-          service_name,
-          price,
+  const { error } = calendarValidator.validate(req.body);
+  const valid = error == null;
+
+  if (valid) {
+    const newSchedule = await prismaClient.schedules.update({
+      where: { id },
+      data: {
+        client,
+        gender,
+        date,
+        time,
+        service: {
+          create: {
+            service_name,
+            price,
+          },
         },
       },
-    },
-  });
+    });
 
-  return res.status(200).json(newSchedule);
+    return res.status(200).json(newSchedule);
+  } else {
+    console.log('Invalido');
+    const { details } = error;
+    //@ts-ignore
+    const message = details.map(i => i.message).join(',');
+
+    return res.status(422).json({ error: message });
+  }
 };
 
 export const deleteSchedule = async (req: Request, res: Response) => {
