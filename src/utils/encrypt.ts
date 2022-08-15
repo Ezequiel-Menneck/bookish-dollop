fc32e54157424fe924398fb0191938f868982e0c
menneck <zeeeee.peeeeetryyy@gmail.com>
2022-08-14 22:33:09 -0300

    feat: add bcrypt to incrypt pass and decrypt

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,8 +1,7 @@
 import bcrypt from 'bcrypt';
-import config from '../config/config';
 
 const encryptPassword = async (password: string) => {
-  const salt = await bcrypt.genSalt(Number(config.SALT));
+  const salt = await bcrypt.genSalt(Number(process.env.SALT));
   return bcrypt.hash(password, salt);
 };
 
